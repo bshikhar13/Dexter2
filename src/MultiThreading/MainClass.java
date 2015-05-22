6d829b624e83901a4427b4b599917fdb5d9a482d
bshikhar13 <bshikhar13131313@gmail.com>
2015-05-22 14:36:18 +0530

    First Thread commit

---
+++ src/MultiThreading/MainClass.java
@@ -0,0 +1,11 @@
+package MultiThreading;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		Multi t1 = new Multi();
+		t1.start();
+
+	}
+
+}
---
+++ src/MultiThreading/Multi.java
@@ -0,0 +1,7 @@
+package MultiThreading;
+
+public class Multi extends Thread{
+	public void run(){
+		System.out.println("The Thread is Running");
+	}
+}
